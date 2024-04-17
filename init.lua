599d4138f3da47dc3127d631d6b1abe392072feb
Tiago Pessoa <70546713+tcpessoa@users.noreply.github.com>
2024-04-18 00:22:23 +0400

    feat: disable autosave for js, jsx, ts, tsx

--- init.lua
+++ init.lua
@@ -620,7 +620,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, javascript = true, typescript = true, typescriptreact = true, javascriptreact = true }
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -633,7 +633,7 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        -- javascript = { { 'prettierd', 'prettier' } },
       },
     },
   },
