2a413b2bf91ce12267db3a669305e5bbb0efdadc
Tiago Pessoa <tiago.pessoa@g42.ai>
2024-04-19 10:34:45 +0400

    feat: update neotree cfg

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -6,8 +6,44 @@ return {
     { 'nvim-tree/nvim-web-devicons', opts = {} },
     'MunifTanjim/nui.nvim',
   },
+  -- this is to disable the default netrw behavior, taken from:
+  -- [github issue](https://github.com/nvim-neo-tree/neo-tree.nvim/issues/1247)
+  init = function()
+    -- the remote file handling part
+    vim.api.nvim_create_autocmd('BufEnter', {
+      group = vim.api.nvim_create_augroup('RemoteFileInit', { clear = true }),
+      callback = function()
+        local f = vim.fn.expand '%:p'
+        for _, v in ipairs { 'dav', 'fetch', 'ftp', 'http', 'rcp', 'rsync', 'scp', 'sftp' } do
+          local p = v .. '://'
+          if f:sub(1, #p) == p then
+            vim.cmd [[
+              unlet g:loaded_netrw
+              unlet g:loaded_netrwPlugin
+              runtime! plugin/netrwPlugin.vim
+              silent Explore %
+            ]]
+            break
+          end
+        end
+        vim.api.nvim_clear_autocmds { group = 'RemoteFileInit' }
+      end,
+    })
+    vim.api.nvim_create_autocmd('BufEnter', {
+      group = vim.api.nvim_create_augroup('NeoTreeInit', { clear = true }),
+      callback = function()
+        local f = vim.fn.expand '%:p'
+        if vim.fn.isdirectory(f) ~= 0 then
+          vim.cmd('Neotree current dir=' .. f)
+          vim.api.nvim_clear_autocmds { group = 'NeoTreeInit' }
+        end
+      end,
+    })
+    -- keymaps
+  end,
   config = function()
     require('neo-tree').setup {
+      close_if_last_window = true,
       filesystem = {
         filtered_items = {
           visible = true,
@@ -15,6 +51,9 @@ return {
         },
         follow_current_file = { enabled = true },
         hijack_netrw_behavior = 'open_current',
+      },
+      window = {
+        position = 'right',
       },
     }
   end,
