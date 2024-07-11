2ba577460592f7e61eff11b3a6e143d63b9e425e
Tiago Pessoa <70546713+tcpessoa@users.noreply.github.com>
2024-07-11 14:40:02 +0400

    feat: better mappings for CopilotChat

--- lua/custom/plugins/copilot-chat.lua
+++ lua/custom/plugins/copilot-chat.lua
@@ -7,7 +7,40 @@ return {
       { 'nvim-lua/plenary.nvim' },
     },
     opts = {
-      debug = true,
+      mappings = {
+        complete = {
+          detail = 'Use <C-y> to insert the selected completion',
+          insert = 'C-y',
+        },
+        close = {
+          normal = 'q',
+          insert = '<C-c>',
+        },
+        reset = {
+          normal = '<C-x>',
+          insert = '<C-x>',
+        },
+        submit_prompt = {
+          normal = '<CR>',
+          insert = '<C-m>',
+        },
+        accept_diff = {
+          normal = '<C-i>',
+          insert = '<C-i>',
+        },
+        yank_diff = {
+          normal = 'gy',
+        },
+        show_diff = {
+          normal = 'gd',
+        },
+        show_system_prompt = {
+          normal = 'gp',
+        },
+        show_user_selection = {
+          normal = 'gs',
+        },
+      },
     },
   },
 }
