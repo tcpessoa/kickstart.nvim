d047970feb6836ba6d3865586ff1c5da2fb27daa
Tiago Pessoa <70546713+tcpessoa@users.noreply.github.com>
2024-09-03 16:07:33 +0400

    fix: which key

--- init.lua
+++ init.lua
@@ -312,37 +312,20 @@ require('lazy').setup({
         },
       },
 
-      -- Document existing key chains
-      require('which-key').add {
+      spec = {
         { '<leader>b', group = '[B]uffers' },
-        { '<leader>b_', hidden = true },
         { '<leader>c', group = '[C]ode' },
-        { '<leader>c_', hidden = true },
         { '<leader>d', group = '[D]ocument' },
-        { '<leader>d_', hidden = true },
-        { '<leader>h', group = 'Git [H]unk' },
-        { '<leader>h_', hidden = true },
-        { '<leader>l', group = 'Uti[L]s' },
-        { '<leader>l_', hidden = true },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>l', group = 'Uti[L]s', mode = { 'n', 'v' } },
         { '<leader>p', group = '[P]ackages' },
-        { '<leader>p_', hidden = true },
         { '<leader>r', group = '[R]ename' },
-        { '<leader>r_', hidden = true },
         { '<leader>s', group = '[S]earch' },
-        { '<leader>s_', hidden = true },
         { '<leader>t', group = '[T]oggle' },
-        { '<leader>t_', hidden = true },
         { '<leader>w', group = '[W]orkspace' },
-        { '<leader>w_', hidden = true },
         { '<leader>x', group = '[X] - Trouble' },
-        { '<leader>x_', hidden = true },
-      }
-      -- visual mode
-      require('which-key').add {
-        { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
-        { '<leader>l', desc = 'Uti[L]s', mode = 'v' },
-      }
-    end,
+      },
+    },
   },
 
   -- NOTE: Plugins can specify dependencies.
