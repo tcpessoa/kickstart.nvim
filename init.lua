90cbfab3b3812ac116cd5e1964ef5b44a774e727
Tiago Pessoa <70546713+tcpessoa@users.noreply.github.com>
2024-09-05 19:59:45 +0400

    feat: remove autosave

--- init.lua
+++ init.lua
@@ -710,7 +710,8 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes =
+          { c = true, cpp = true, javascript = true, typescript = true, json = true, javascriptreact = true, typescriptreact = true, yaml = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
