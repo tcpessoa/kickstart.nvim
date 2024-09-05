1319977a6903f22f4f8347922cd2788f3a183f3d
Tiago Pessoa <70546713+tcpessoa@users.noreply.github.com>
2024-09-05 21:19:29 +0400

    feat: add avante

---
+++ lua/custom/plugins/avante.lua
@@ -0,0 +1,27 @@
+return {
+  'yetone/avante.nvim',
+  event = 'VeryLazy',
+  lazy = false,
+  opts = {
+    -- add any opts here
+  },
+  -- if you want to download pre-built binary, then pass source=false. Make sure to follow instruction above.
+  -- Also note that downloading prebuilt binary is a lot faster comparing to compiling from source.
+  build = ':AvanteBuild source=false',
+  dependencies = {
+    'stevearc/dressing.nvim',
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    --- The below dependencies are optional,
+    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+    'zbirenbaum/copilot.lua', -- for providers='copilot'
+    {
+      -- Make sure to setup it properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { 'markdown', 'Avante' },
+      },
+      ft = { 'markdown', 'Avante' },
+    },
+  },
+}
