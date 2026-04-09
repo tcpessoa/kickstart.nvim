26cb0593e58814d7afa170ff105da0a2650f4bb7
Tiago Pessoa <70546713+tcpessoa@users.noreply.github.com>
2026-04-09 13:51:54 +0400

    feat(diffview): update default conflict view

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,3 +1,13 @@
 return {
   'sindrets/diffview.nvim',
+  opts = {
+    enhanced_diff_hl = true,
+    view = {
+      merge_tool = { layout = 'diff4_mixed' },
+    },
+    file_panel = {
+      listing_style = 'tree',
+      win_config = { width = 35 },
+    },
+  },
 }
