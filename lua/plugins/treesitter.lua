f2067a73d896a69784c95743124e5ecdc2b718cf
Tiago Pessoa <70546713+tcpessoa@users.noreply.github.com>
2026-04-13 16:04:16 +0400

    fix(treesitter): accomodate treesiter to neovim 0.12+

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,55 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
+  lazy = false,
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs',
-  opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-  },
+  config = function()
+    require('nvim-treesitter').setup {}
+
+    -- Install parsers (no-op if already installed)
+    require('nvim-treesitter').install {
+      'bash',
+      'c',
+      'cpp',
+      'css',
+      'diff',
+      'dockerfile',
+      'go',
+      'html',
+      'javascript',
+      'jsdoc',
+      'json',
+      'lua',
+      'luadoc',
+      'markdown',
+      'markdown_inline',
+      'python',
+      'query',
+      'ruby',
+      'sql',
+      'toml',
+      'tsx',
+      'typescript',
+      'vim',
+      'vimdoc',
+      'xml',
+      'yaml',
+    }
+
+    -- Treesitter highlighting is now built into Neovim.
+    -- Enable it via FileType autocmd.
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
+
+    -- Treesitter-based indentation
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'ruby' },
+      callback = function()
+        vim.bo.indentexpr = ''
+      end,
+    })
+  end,
 }
